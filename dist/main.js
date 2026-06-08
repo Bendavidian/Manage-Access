3672bca4fb327c94f361912f21f9bec9229863ed
dani020799 <46074662+dani020799@users.noreply.github.com>
2026-06-08 12:38:47 +0300

    pull request back end
    pull request back end

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,6 @@
 const secretMessage = document.querySelector("#secret-message");
 
-secretMessage.textContent = "This is Partner A's secret message from the front-end!";
+//secretMessage.textContent = "This is Partner A's secret message from the front-end!";
+
+//hello
+
